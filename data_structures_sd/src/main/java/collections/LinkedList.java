5efc145d42f9fca2f00003190e1b6693049cece0
michelle <michelle.graham@dkit.ie>
2023-02-26 22:27:04 +0000

    Added addFirst() and get() methods to structure.

--- data_structures_sd/src/main/java/collections/LinkedList.java
+++ data_structures_sd/src/main/java/collections/LinkedList.java
@@ -28,6 +28,29 @@ public class LinkedList {
         return size;
     }
 
+    public boolean addFirst(int value) {
+        Node newNode = new Node(value);
+        if (first == null) {
+            first = newNode;
+        } else {
+            newNode.next = first;
+            first = newNode;
+        }
+        size++;
+        return true;
+    }
+
+    public int get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid position provided");
+        }
+        Node current = first;
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current.data;
+    }
+
     private static class Node {
 
         private int data;
