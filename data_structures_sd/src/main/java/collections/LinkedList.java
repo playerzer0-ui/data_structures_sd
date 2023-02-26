560043bedc155070de35d5e67e9b1438289221ef
michelle <michelle.graham@dkit.ie>
2023-02-26 22:30:34 +0000

    Added add() and add in position method.

--- data_structures_sd/src/main/java/collections/LinkedList.java
+++ data_structures_sd/src/main/java/collections/LinkedList.java
@@ -51,6 +51,41 @@ public class LinkedList {
         return current.data;
     }
 
+    public boolean add(int value) {
+        Node newNode = new Node(value);
+        if (first == null) {
+            first = newNode;
+        } else {
+            Node current = first;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        size++;
+        return true;
+    }
+
+    public boolean add(int value, int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Invalid position provided");
+        }
+
+        if (index == 0) {
+            return addFirst(value);
+        }
+
+        Node current = first;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        Node newNode = new Node(value);
+        newNode.next = current.next;
+        current.next = newNode;
+        size++;
+        return true;
+    }
+
     private static class Node {
 
         private int data;
