70be4845cea7591b7a9ee4c3f829fe0fe18d001c
michelle <michelle.graham@dkit.ie>
2023-02-26 22:26:05 +0000

    Added clear(), isEmpty() and size() to base structure.

--- data_structures_sd/src/main/java/collections/LinkedList.java
+++ data_structures_sd/src/main/java/collections/LinkedList.java
@@ -5,19 +5,35 @@ package collections;
  * @author michelle
  */
 public class LinkedList {
+
     private Node first;
     private int size;
-    
-    public LinkedList(){
+
+    public LinkedList() {
         first = null;
         size = 0;
     }
-    
-    private static class Node{
+
+    public void clear() {
+        first = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+        // return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private static class Node {
+
         private int data;
         private Node next;
-        
-        public Node(int value){
+
+        public Node(int value) {
             this.data = value;
             this.next = null;
         }
